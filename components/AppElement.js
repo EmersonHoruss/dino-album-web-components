dfb45d730dae659db661b98233c53e168a8aa4cf
tristanaTalon <peralesvillanuevaemerson@gmail.com>
2023-02-11 00:13:38 -0500

    testing methods releated with props

--- components/AppElement.js
+++ components/AppElement.js
@@ -1,6 +1,9 @@
 class AppElement extends HTMLElement {
     constructor() {
         super()
+        console.log("The component has next attributes: ", this.getAttributeNames())
+        console.log("Value is: ", this.getAttribute("value"))
+        console.log("Exist isEnabled attribute?: ", this.hasAttribute('isEnabled'))
     }
 }
 
