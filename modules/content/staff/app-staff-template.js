131a14ebbdbfdc8c036cf61352f145631820dc64
Emerson Perales <peralesvillanuevaemerson@gmail.com>
2023-03-18 03:17:33 -0500

    app staff styles

--- modules/content/staff/app-staff-template.js
+++ modules/content/staff/app-staff-template.js
@@ -3,8 +3,6 @@ const template = document.createElement("template")
 template.innerHTML = `
 <div id="staff-wrapper" class="wrapper">
     <div id="staff"></div>
-    lorem*10
-
     <app-paginator totalPages="0"></app-paginator>
 <div/>
 `
